dca2ec0abeda4860c5bc2be32397a90801786277
Romaine Murray <romainemurray6@gmail.com>
2020-04-29 14:22:32 -0500

    Fixed a typo

--- app/models.py
+++ app/models.py
@@ -72,7 +72,7 @@ class Likes(db.Model):
     __tablename__ = 'likes'
 
     id = db.Column(db.Integer,primary_key=True)
-    user_id = db.Column(db.Integer,db.ForeginKey('users.id'),primary_key=True,nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),primary_key=True,nullable=False)
     post_id = db.Column(db.Integer,db.ForeignKey('posts.id'),nullable=False)
 
 
